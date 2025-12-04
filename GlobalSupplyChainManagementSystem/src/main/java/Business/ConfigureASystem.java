0daea2c83bb272f13792c622e3464fa29a8e1ad0
sangeetasingh1 <singh.sang@northeastern.edu>
2025-12-03 22:55:25 -0500

    Code changes for configuring a system a system Admin business file

---
+++ GlobalSupplyChainManagementSystem/src/main/java/Business/ConfigureASystem.java
@@ -0,0 +1,34 @@
+package Business;
+
+import Business.DataGenerator.DataGenerator;
+import Business.Employee.Employee;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
+import Business.Util.PasswordUtil;
+
+/**
+ * 
+ * @author sange
+ */
+public class ConfigureASystem {
+    
+    public static SupplyChainSystem configure(){
+        
+        SupplyChainSystem system = SupplyChainSystem.getInstance();
+        
+        // Create system admin
+        Employee employee = system.getEmployeeDirectory().createEmployee("System Administrator");
+        employee.setEmail("admin@ecosystem.com");
+        employee.setAge(35);
+        employee.setPhoneNumber("555-0001");
+        
+        String hashedPassword = PasswordUtil.hashPassword("admin123");
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", hashedPassword, employee, new SystemAdminRole());
+        
+        // Populate system with test data using Faker
+        DataGenerator.populateSystem(system);
+        
+        return system;
+    }
+    
+}
