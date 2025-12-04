da83030f474d51dafb3f7a4f259127be75d8a09e
sangeetasingh1 <singh.sang@northeastern.edu>
2025-12-03 23:13:43 -0500

    Creation of Employee and Employee Directory

---
+++ GlobalSupplyChainManagementSystem/src/main/java/Business/Employee/Employee.java
@@ -0,0 +1,61 @@
+package Business.Employee;
+
+/**
+ *
+ * @author sange
+ */
+public class Employee {
+    
+    private String name;
+    private int id;
+    private String email;
+    private int age;
+    private String phoneNumber;
+    private static int count = 1;
+
+    public Employee() {
+        id = count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
