bb99094633ec05c419360bf7ce4313a61c7ee500
sangeetasingh1 <singh.sang@northeastern.edu>
2025-12-03 23:05:31 -0500

    Creating the Organization directory to get the Organization list based on Type

---
+++ GlobalSupplyChainManagementSystem/src/main/java/Business/Organization/OrganizationDirectory.java
@@ -0,0 +1,62 @@
+package Business.Organization;
+
+import Business.Organization.Organization.Type;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sange
+ */
+public class OrganizationDirectory {
+    
+    private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+    
+    public Organization createOrganization(Type type){
+        Organization organization = null;
+        if (type.getValue().equals(Type.Doctor.getValue())){
+            organization = new DoctorOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Lab.getValue())){
+            organization = new LabOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Admin.getValue())){
+            organization = new AdminOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Production.getValue())){
+            organization = new ProductionOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.QualityControl.getValue())){
+            organization = new QualityControlOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Warehouse.getValue())){
+            organization = new WarehouseOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Shipping.getValue())){
+            organization = new ShippingOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Inventory.getValue())){
+            organization = new InventoryOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Sales.getValue())){
+            organization = new SalesOrganization();
+            organizationList.add(organization);
+        }
+        return organization;
+    }
+}
